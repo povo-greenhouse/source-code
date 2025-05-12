dfb1c60ba3da76a6924f5c4f6be5983beea22c36
riginel <rigiungureanu@gmail.com>
2025-05-12 10:00:53 +0200

    added scheduling module. not tested yet

---
+++ scheduling/scheduler.c
@@ -0,0 +1,98 @@
+/*
+ * scheduler.c
+ *
+ *  Created on: May 9, 2025
+ *      Author: riginel
+ */
+
+#include "scheduling/scheduler.h"
+
+
+
+
+
+void init_task_list(){
+    task_list.curr = 0;
+}
+
+
+int push_task(STask task){
+   if (task_list.curr < N_PERIODIC_TASKS ){
+       task_list.task_array[task_list.curr] = task;
+
+       return task_list.curr++;
+   }
+   return 0;
+}
+
+int pop_task(){
+    if (task_list.curr >0){
+        task_list.curr -=1;
+        return 1;
+
+    }
+    return 0;
+}
+
+
+
+
+
+void init_task_queue(){
+    task_queue.write_index = 0;
+    task_queue.read_index = 0;
+}
+//enqueues the given task
+int enqueue_task(STask * task) {
+    if((task_queue.write_index + 1) % QUEUE_CAPACITY == task_queue.read_index){
+        return 0;
+    }
+    task_queue.arr[task_queue.write_index] = task->fpointer;
+    task_queue.write_index = (task_queue.write_index + 1) % QUEUE_CAPACITY;
+    return 1;
+}
+//dequeues the last task, returning it
+TaskFP dequeue_task(){
+    if(task_queue.read_index == task_queue.write_index){
+        return 0;
+    }
+    TaskFP ret =  task_queue.arr[task_queue.read_index];
+    task_queue.read_index = (task_queue.read_index + 1) % QUEUE_CAPACITY;
+    return ret;
+
+}
+
+
+
+
+void scheduler(){
+    TaskFP next = dequeue_task();
+    while(next != 0){
+        next();
+    }
+    scheduler_state = SLEEPING;
+}
+
+void timer_interrupt(int elapsed){
+    int scheduled_at_least_once = 0;
+    int i;
+    for(i = 0; i < task_list.curr;i++){
+        STask * t = &(task_list.task_array[i]);
+        if(!t->is_active){
+            continue;
+        }
+        t->elapsed_time -= elapsed;
+        if (t->elapsed_time <= 0){
+            t->elapsed_time = t->max_time;
+            //time to schedule
+            enqueue_task(t);
+            scheduled_at_least_once =1;
+        }
+    }
+    if(scheduled_at_least_once && scheduler_state==SLEEPING){
+        scheduler_state = AWAKE;
+        scheduler();
+    }
+
+}
+
---
+++ scheduling/scheduler.h
@@ -0,0 +1,127 @@
+/*
+ * scheduler.h
+ *
+ *  Created on: May 10, 2025
+ *      Author: riginel
+ */
+
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+#include "msp.h"
+#include "stdint.h"
+/*
+    pointer to routines
+ */
+typedef void (*TaskFP)(void);
+/*
+    struct representing a task
+    fields:
+    - fpointer: pointer to the routine
+    - max_time: period of the task in milliseconds
+    - elapsed_time: time elapsed since last time in milliseconds
+    - is_active: indicates whether or not this task is active
+*/
+typedef struct{
+    TaskFP fpointer;
+    int32_t max_time;
+    int32_t elapsed_time;
+    int32_t is_active;
+} STask;
+
+
+#define N_PERIODIC_TASKS 10
+/*
+    List of the tasks that are periodically executed, used as a stack
+    fields:
+    - task_array: the underlying array
+    - curr: index of next available slot;
+
+*/
+typedef struct {
+    STask task_array[N_PERIODIC_TASKS];
+    int32_t curr;
+}STaskList;
+
+// global task list
+STaskList task_list;
+
+
+/*
+    initializes the task list
+*/
+void init_task_list();
+
+
+/*
+pushes the given task on the list
+arguments:
+    - task: the task to be pushed
+returns:
+    - -1 if there's no space
+    - the index of the newly placed task in the array
+
+ */
+int push_task(STask task);
+/*
+removes the last task on the list
+*/
+int pop_task();
+
+
+
+
+
+
+#define QUEUE_CAPACITY 50
+/*
+    scheduler's task queue, as a circular buffer
+    fields:
+    - arr: underlying array
+    - write_index: index where to write
+    - read_index: index where to read
+*/
+typedef struct {
+    TaskFP arr[QUEUE_CAPACITY];
+    int write_index;
+    int read_index;
+}STaskQueue;
+
+//global task queue
+STaskQueue task_queue;
+
+
+// initializes the task queue
+void init_task_queue();
+
+
+//enqueues the given task
+int enqueue_task(STask * task);
+//dequeues the last task, returning it
+TaskFP dequeue_task();
+/*
+    enum representing the state of the scheduler
+    SLEEPING: no tasks scheduled
+    AWAKE: currently scheduling tasks
+*/
+typedef enum {
+    SLEEPING,
+    AWAKE
+} SState;
+
+SState scheduler_state;
+
+void scheduler();
+
+/*
+    function called when timer sets off
+    subtracts specified milliseconds from tasks' elapsed field and schedules them when it gets to 0
+
+    arguments:
+    - elapsed: elapsed milliseconds since last timer interrupt
+ */
+void timer_interrupt(int elapsed);
+
+
+
+
+#endif /* SCHEDULER_H_ */
